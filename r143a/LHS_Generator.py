fa339de401fe7af47351530ead39b21a5eff7cd6
Montana Carlozo <mcarlozo@nd.edu>
2022-05-26 15:08:58 -0400

    Added LHS generator and csv from local machine

---
+++ r143a/LHS_Generator.py
@@ -0,0 +1,13 @@
+from scipy.stats import qmc
+import csv
+
+d= 8 #Number of dimensions
+n = 200 ## of samples
+sampler = qmc.LatinHypercube(d)
+sample = sampler.random(n)
+print(sample)
+
+
+with open('LHS'+str(n)+'x'+str(d)+'.csv', 'w', newline='') as file:
+    mywriter = csv.writer(file, delimiter=',')
+    mywriter.writerows(sample)
